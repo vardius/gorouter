b6a4ce4e292367cb9a88e4ca9eab925ab1c49b71
Rafał Lorenz <vardius@gmail.com>
2016-08-01 11:30:39 +0200

    Fix routes for path /

--- context.go
+++ context.go
@@ -12,9 +12,12 @@ type Context struct {
 }
 
 func fromRequest(r *route, req *http.Request) (*Context, error) {
-	paths := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
-	route, params := r.getRoute(paths)
+	var paths []string
+	if path := strings.Trim(req.URL.Path, "/"); path != "" {
+		paths = strings.Split(path, "/")
+	}
 
+	route, params := r.getRoute(paths)
 	if route != nil {
 		return &Context{params, route}, nil
 	}

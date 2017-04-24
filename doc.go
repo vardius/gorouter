060ab065bf99f5878e0e8c7e7ab9e5f2a859071a
Rafał Lorenz <vardius@gmail.com>
2017-04-24 23:15:09 +1000

    Removed markdown from docs

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
 Package goserver provide request router with middleware
 
-## Basic example
+Basic example
 
 	package main
 
@@ -30,36 +30,31 @@ Package goserver provide request router with middleware
 		log.Fatal(http.ListenAndServe(":8080", server))
 	}
 
-## Routing
-
+Routing
 The router determines how to handle that request.
 Goserver uses a routing tree. Once one branch of the tree matches, only routes inside that branch are considered,
 not any routes after that branch. When instantiating server, the root node of router tree is created.
 
-### Route types
+Route types
+- Static `/hello`
+will match requests matching given route
+- Named `/:name`
+will match requests matching given route scheme
+- Regexp `/:name:[a-z]+`
+will match requests matching given route scheme and its regexp
 
-	- Static `/hello`
-	will match requests matching given route
-	- Named `/:name`
-	will match requests matching given route scheme
-	- Regexp `/:name:[a-z]+`
-	will match requests matching given route scheme and its regexp
-
-#### Wildcards
-
+Wildcards
 The values of *named parameter* or *regexp parameters* are accessible via *request context*
 `params, ok := goserver.ParamsFromContext(req.Context())`.
 You can get the value of a parameter either by its index in the slice, or by using the `params.Value(name)` method:
 `:name` or `/:name:[a-z]+` can be retrived by `params.Value("name")`.
 
-### Defining Routes
-
+Defining Routes
 A full route definition contain up to three parts:
-
-	1. HTTP method under which route will be available
-	2. The URL path route. This is matched against the URL passed to the server,
-	and can contain named wildcard placeholders *(e.g. :placeholders)* to match dynamic parts in the URL.
-	3. `http.HandleFunc`, which tells the server to handle matched requests to the router with handler.
+1. HTTP method under which route will be available
+2. The URL path route. This is matched against the URL passed to the server,
+and can contain named wildcard placeholders *(e.g. :placeholders)* to match dynamic parts in the URL.
+3. `http.HandleFunc`, which tells the server to handle matched requests to the router with handler.
 
 Take the following example:
 

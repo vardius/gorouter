d8fbe3b412e392c7cae06c4ef3e29859c16ede0f
Rafał Lorenz <vardius@gmail.com>
2019-08-31 09:18:59 +1000

    Add more benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -4,16 +4,18 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/valyala/fasthttp"
 )
 
-func benchmarkStaticCall(t int, b *testing.B) {
+func benchmarkStatic(t int, b *testing.B) {
 	var path string
 	part := "/x"
 	for i := 0; i < t; i++ {
 		path += part
 	}
 
-	s := New().(*router)
+	s := New()
 	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
 
 	w := httptest.NewRecorder()
@@ -23,36 +25,107 @@ func benchmarkStaticCall(t int, b *testing.B) {
 	}
 
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		s.ServeHTTP(w, req)
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.ServeHTTP(w, req)
+		}
+	})
 }
 
-func benchmarkStaticParallel(t int, b *testing.B) {
+func benchmarkWildcard(t int, b *testing.B) {
+	var path, rpath string
+	part := "/{x}"
+	rpart := "/x"
+	for i := 0; i < t; i++ {
+		path += part
+		rpath += rpart
+	}
+
+	s := New()
+	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(GET, rpath, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.ServeHTTP(w, req)
+		}
+	})
+}
+
+func benchmarkRegexp(t int, b *testing.B) {
+	var path, rpath string
+	part := "/{x:r([a-z]+)go}"
+	rpart := "/rxgo"
+	for i := 0; i < t; i++ {
+		path += part
+		rpath += rpart
+	}
+
+	s := New()
+	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(GET, rpath, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			s.ServeHTTP(w, req)
+		}
+	})
+}
+
+func BenchmarkStatic1(b *testing.B)  { benchmarkStatic(1, b) }
+func BenchmarkStatic2(b *testing.B)  { benchmarkStatic(2, b) }
+func BenchmarkStatic3(b *testing.B)  { benchmarkStatic(3, b) }
+func BenchmarkStatic5(b *testing.B)  { benchmarkStatic(5, b) }
+func BenchmarkStatic10(b *testing.B) { benchmarkStatic(10, b) }
+func BenchmarkStatic20(b *testing.B) { benchmarkStatic(20, b) }
+
+func BenchmarkWildcard1(b *testing.B)  { benchmarkWildcard(1, b) }
+func BenchmarkWildcard2(b *testing.B)  { benchmarkWildcard(2, b) }
+func BenchmarkWildcard3(b *testing.B)  { benchmarkWildcard(3, b) }
+func BenchmarkWildcard5(b *testing.B)  { benchmarkWildcard(5, b) }
+func BenchmarkWildcard10(b *testing.B) { benchmarkWildcard(10, b) }
+func BenchmarkWildcard20(b *testing.B) { benchmarkWildcard(20, b) }
+
+func BenchmarkRegexp1(b *testing.B)  { benchmarkRegexp(1, b) }
+func BenchmarkRegexp2(b *testing.B)  { benchmarkRegexp(2, b) }
+func BenchmarkRegexp3(b *testing.B)  { benchmarkRegexp(3, b) }
+func BenchmarkRegexp5(b *testing.B)  { benchmarkRegexp(5, b) }
+func BenchmarkRegexp10(b *testing.B) { benchmarkRegexp(10, b) }
+func BenchmarkRegexp20(b *testing.B) { benchmarkRegexp(20, b) }
+
+func benchmarkFastHTTPStatic(t int, b *testing.B) {
 	var path string
 	part := "/x"
 	for i := 0; i < t; i++ {
 		path += part
 	}
 
-	s := New().(*router)
-	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+	s := NewFastHTTPRouter()
+	s.GET(path, func(_ *fasthttp.RequestCtx) {})
 
-	req, err := http.NewRequest(GET, path, nil)
-	if err != nil {
-		b.Fatal(err)
-	}
+	ctx := buildFastHTTPRequestContext(GET, path)
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
-		w := httptest.NewRecorder()
 		for pb.Next() {
-			s.ServeHTTP(w, req)
+			s.HandleFastHTTP(ctx)
 		}
 	})
 }
 
-func benchmarkWildcardCall(t int, b *testing.B) {
+func benchmarkFastHTTPWildcard(t int, b *testing.B) {
 	var path, rpath string
 	part := "/{x}"
 	rpart := "/x"
@@ -61,48 +134,20 @@ func benchmarkWildcardCall(t int, b *testing.B) {
 		rpath += rpart
 	}
 
-	s := New().(*router)
-	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+	s := NewFastHTTPRouter()
+	s.GET(path, func(_ *fasthttp.RequestCtx) {})
 
-	w := httptest.NewRecorder()
-	req, err := http.NewRequest(GET, rpath, nil)
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		s.ServeHTTP(w, req)
-	}
-}
-
-func benchmarkWildcardParallel(t int, b *testing.B) {
-	var path, rpath string
-	part := "/{x}"
-	rpart := "/x"
-	for i := 0; i < t; i++ {
-		path += part
-		rpath += rpart
-	}
-
-	s := New().(*router)
-	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
-
-	req, err := http.NewRequest(GET, rpath, nil)
-	if err != nil {
-		b.Fatal(err)
-	}
+	ctx := buildFastHTTPRequestContext(GET, rpath)
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
-		w := httptest.NewRecorder()
 		for pb.Next() {
-			s.ServeHTTP(w, req)
+			s.HandleFastHTTP(ctx)
 		}
 	})
 }
 
-func benchmarkRegexpCall(t int, b *testing.B) {
+func benchmarkFastHTTPRegexp(t int, b *testing.B) {
 	var path, rpath string
 	part := "/{x:r([a-z]+)go}"
 	rpart := "/rxgo"
@@ -111,85 +156,36 @@ func benchmarkRegexpCall(t int, b *testing.B) {
 		rpath += rpart
 	}
 
-	s := New().(*router)
-	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+	s := NewFastHTTPRouter()
+	s.GET(path, func(_ *fasthttp.RequestCtx) {})
 
-	w := httptest.NewRecorder()
-	req, err := http.NewRequest(GET, rpath, nil)
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		s.ServeHTTP(w, req)
-	}
-}
-
-func benchmarkRegexpParallel(t int, b *testing.B) {
-	var path, rpath string
-	part := "/{x:r([a-z]+)go}"
-	rpart := "/rxgo"
-	for i := 0; i < t; i++ {
-		path += part
-		rpath += rpart
-	}
-
-	s := New().(*router)
-	s.GET(path, http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
-
-	req, err := http.NewRequest(GET, rpath, nil)
-	if err != nil {
-		b.Fatal(err)
-	}
+	ctx := buildFastHTTPRequestContext(GET, rpath)
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
-		w := httptest.NewRecorder()
 		for pb.Next() {
-			s.ServeHTTP(w, req)
+			s.HandleFastHTTP(ctx)
 		}
 	})
 }
 
-func BenchmarkStatic1(b *testing.B)  { benchmarkStaticCall(1, b) }
-func BenchmarkStatic2(b *testing.B)  { benchmarkStaticCall(2, b) }
-func BenchmarkStatic3(b *testing.B)  { benchmarkStaticCall(3, b) }
-func BenchmarkStatic5(b *testing.B)  { benchmarkStaticCall(5, b) }
-func BenchmarkStatic10(b *testing.B) { benchmarkStaticCall(10, b) }
-func BenchmarkStatic20(b *testing.B) { benchmarkStaticCall(20, b) }
+func BenchmarkFastHTTPStatic1(b *testing.B)  { benchmarkFastHTTPStatic(1, b) }
+func BenchmarkFastHTTPStatic2(b *testing.B)  { benchmarkFastHTTPStatic(2, b) }
+func BenchmarkFastHTTPStatic3(b *testing.B)  { benchmarkFastHTTPStatic(3, b) }
+func BenchmarkFastHTTPStatic5(b *testing.B)  { benchmarkFastHTTPStatic(5, b) }
+func BenchmarkFastHTTPStatic10(b *testing.B) { benchmarkFastHTTPStatic(10, b) }
+func BenchmarkFastHTTPStatic20(b *testing.B) { benchmarkFastHTTPStatic(20, b) }
 
-func BenchmarkWildcard1(b *testing.B)  { benchmarkWildcardCall(1, b) }
-func BenchmarkWildcard2(b *testing.B)  { benchmarkWildcardCall(2, b) }
-func BenchmarkWildcard3(b *testing.B)  { benchmarkWildcardCall(3, b) }
-func BenchmarkWildcard5(b *testing.B)  { benchmarkWildcardCall(5, b) }
-func BenchmarkWildcard10(b *testing.B) { benchmarkWildcardCall(10, b) }
-func BenchmarkWildcard20(b *testing.B) { benchmarkWildcardCall(20, b) }
+func BenchmarkFastHTTPWildcard1(b *testing.B)  { benchmarkFastHTTPWildcard(1, b) }
+func BenchmarkFastHTTPWildcard2(b *testing.B)  { benchmarkFastHTTPWildcard(2, b) }
+func BenchmarkFastHTTPWildcard3(b *testing.B)  { benchmarkFastHTTPWildcard(3, b) }
+func BenchmarkFastHTTPWildcard5(b *testing.B)  { benchmarkFastHTTPWildcard(5, b) }
+func BenchmarkFastHTTPWildcard10(b *testing.B) { benchmarkFastHTTPWildcard(10, b) }
+func BenchmarkFastHTTPWildcard20(b *testing.B) { benchmarkFastHTTPWildcard(20, b) }
 
-func BenchmarkRegexp1(b *testing.B)  { benchmarkRegexpCall(1, b) }
-func BenchmarkRegexp2(b *testing.B)  { benchmarkRegexpCall(2, b) }
-func BenchmarkRegexp3(b *testing.B)  { benchmarkRegexpCall(3, b) }
-func BenchmarkRegexp5(b *testing.B)  { benchmarkRegexpCall(5, b) }
-func BenchmarkRegexp10(b *testing.B) { benchmarkRegexpCall(10, b) }
-func BenchmarkRegexp20(b *testing.B) { benchmarkRegexpCall(20, b) }
-
-func BenchmarkStaticParallel1(b *testing.B)  { benchmarkStaticParallel(1, b) }
-func BenchmarkStaticParallel2(b *testing.B)  { benchmarkStaticParallel(2, b) }
-func BenchmarkStaticParallel3(b *testing.B)  { benchmarkStaticParallel(3, b) }
-func BenchmarkStaticParallel5(b *testing.B)  { benchmarkStaticParallel(5, b) }
-func BenchmarkStaticParallel10(b *testing.B) { benchmarkStaticParallel(10, b) }
-func BenchmarkStaticParallel20(b *testing.B) { benchmarkStaticParallel(20, b) }
-
-func BenchmarkWildcardParallel1(b *testing.B)  { benchmarkWildcardParallel(1, b) }
-func BenchmarkWildcardParallel2(b *testing.B)  { benchmarkWildcardParallel(2, b) }
-func BenchmarkWildcardParallel3(b *testing.B)  { benchmarkWildcardParallel(3, b) }
-func BenchmarkWildcardParallel5(b *testing.B)  { benchmarkWildcardParallel(5, b) }
-func BenchmarkWildcardParallel10(b *testing.B) { benchmarkWildcardParallel(10, b) }
-func BenchmarkWildcardParallel20(b *testing.B) { benchmarkWildcardParallel(20, b) }
-
-func BenchmarkRegexpParallel1(b *testing.B)  { benchmarkRegexpParallel(1, b) }
-func BenchmarkRegexpParallel2(b *testing.B)  { benchmarkRegexpParallel(2, b) }
-func BenchmarkRegexpParallel3(b *testing.B)  { benchmarkRegexpParallel(3, b) }
-func BenchmarkRegexpParallel5(b *testing.B)  { benchmarkRegexpParallel(5, b) }
-func BenchmarkRegexpParallel10(b *testing.B) { benchmarkRegexpParallel(10, b) }
-func BenchmarkRegexpParallel20(b *testing.B) { benchmarkRegexpParallel(20, b) }
+func BenchmarkFastHTTPRegexp1(b *testing.B)  { benchmarkFastHTTPRegexp(1, b) }
+func BenchmarkFastHTTPRegexp2(b *testing.B)  { benchmarkFastHTTPRegexp(2, b) }
+func BenchmarkFastHTTPRegexp3(b *testing.B)  { benchmarkFastHTTPRegexp(3, b) }
+func BenchmarkFastHTTPRegexp5(b *testing.B)  { benchmarkFastHTTPRegexp(5, b) }
+func BenchmarkFastHTTPRegexp10(b *testing.B) { benchmarkFastHTTPRegexp(10, b) }
+func BenchmarkFastHTTPRegexp20(b *testing.B) { benchmarkFastHTTPRegexp(20, b) }

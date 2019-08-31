686133d04bd1d8758b7c23c1d5f5236c9265abef
Rafał Lorenz <vardius@gmail.com>
2019-08-31 15:54:09 +1000

    Add path tests

--- path/path_test.go
+++ path/path_test.go
@@ -14,12 +14,22 @@ func TestSplit(t *testing.T) {
 		args      args
 		wantParts []string
 	}{
-		// TODO: Add test cases.
+		{"/", args{"/"}, nil},
+		{"/x", args{"/x"}, []string{"x"}},
+		{"/x/", args{"/x/"}, []string{"x"}},
+		{"/x/y", args{"/x/y"}, []string{"x/y"}},
+		{"/x/y/", args{"/x/y/"}, []string{"x/y"}},
+		{"/{name}", args{"/{name}"}, []string{"{name}"}},
+		{"/{name}/", args{"/{name}/"}, []string{"{name}"}},
+		{"/x/{name}", args{"/x/{name}"}, []string{"x", "{name}"}},
+		{"/x/{name}/", args{"/x/{name}/"}, []string{"x", "{name}"}},
+		{"/x/{name}/y", args{"/x/{name}/y"}, []string{"x", "{name}", "y"}},
+		{"/x/{name}/y/", args{"/x/{name}/y/"}, []string{"x", "{name}", "y"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotParts := Split(tt.args.path); !reflect.DeepEqual(gotParts, tt.wantParts) {
-				t.Errorf("Split() = %v, want %v", gotParts, tt.wantParts)
+				t.Errorf("[%s] Split() = %v, want %v", tt.name, gotParts, tt.wantParts)
 			}
 		})
 	}
@@ -34,12 +44,22 @@ func TestTrim(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
+		{"/", args{"/"}, ""},
+		{"/x", args{"/x"}, "x"},
+		{"/x/", args{"/x/"}, "x"},
+		{"/x/y", args{"/x/y"}, "x/y"},
+		{"/x/y/", args{"/x/y/"}, "x/y"},
+		{"/{name}", args{"/{name}"}, "{name}"},
+		{"/{name}/", args{"/{name}/"}, "{name}"},
+		{"/x/{name}", args{"/x/{name}"}, "x/{name}"},
+		{"/x/{name}/", args{"/x/{name}/"}, "x/{name}"},
+		{"/x/{name}/y", args{"/x/{name}/y"}, "x/{name}/y"},
+		{"/x/{name}/y/", args{"/x/{name}/y/"}, "x/{name}/y"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Trim(tt.args.path); got != tt.want {
-				t.Errorf("Trim() = %v, want %v", got, tt.want)
+				t.Errorf("[%s] Trim() = %v, want %v", tt.name, got, tt.want)
 			}
 		})
 	}

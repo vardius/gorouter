01b11f24372cc69adf93932a40ac6238971abdbe
Rafal Lorenz <rafal@productreview.com.au>
2020-02-05 22:25:55 +1100

    Update website

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: "gorouter", // Title for your website.
   tagline: "Go Server/API micro framwework, HTTP request router, multiplexer, mux",
-  url: "https://gorouter.vardius.github.io", // Your website URL
+  url: "http://rafallorenz.com/gorouter/", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',

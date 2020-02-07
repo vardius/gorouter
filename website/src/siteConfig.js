acab0e444d45fa259110cd6687ed1543e8fcba37
Rafał Lorenz <vardius@gmail.com>
2020-02-07 23:53:49 +1100

    Update siteConfig.js

--- website/src/siteConfig.js
+++ website/src/siteConfig.js
@@ -78,7 +78,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} gorouter`,
+  copyright: `Copyright © ${new Date().getFullYear()} Rafał Lorenz`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
